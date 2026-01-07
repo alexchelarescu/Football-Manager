d0678390055cfd2f04965a4f01eceedc9945e21f
Football Manager <toggledrd@gmail.com>
2026-01-07 03:15:57 +0200

    bife

--- HeadereSiCpp/Echipa.h
+++ HeadereSiCpp/Echipa.h
@@ -48,6 +48,7 @@ public:
     Echipa(std::string nume, std::unique_ptr<Tactica> tacticaInitiala, int obiectiv);
     void oferaFeedbackConducere(int pozitieCurenta) const;
 
+    //[[nodiscard]] int getObiectiv() const { return m_locObiectiv; }
     [[nodiscard]] double calculeazaOvrEchipa() const;
     [[nodiscard]] const Jucator& gasesteStarulEchipei() const;
 
